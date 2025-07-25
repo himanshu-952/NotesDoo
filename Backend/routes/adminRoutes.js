b923ececd261cbcd1567f673479c1da61c4914bf
himanshu-952 <himanshukhatri952@gmail.com>
2025-07-25 22:13:55 +0530

    fixed path

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -1,11 +1,9 @@
-
 const express = require("express");
 const router = express.Router();
-const { deleteNote } = require("../controllers/noteController");
+const { deleteNote, getNotes } = require("../controllers/noteController");
 const { protect, isAdmin } = require("../middleware/authMiddleware");
-const {  getNotes } = require("../controllers/noteController");
 
 router.delete("/delete/:id", protect, isAdmin, deleteNote);
-router.get("/notes" ,protect, isAdmin, getNotes)
+router.get("/notes", protect, isAdmin, getNotes);
 
 module.exports = router;
