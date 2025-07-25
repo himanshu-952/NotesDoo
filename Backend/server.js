f9446b5cea071f0c71e7d22c2841bd2c8239bdc7
himanshu-952 <himanshukhatri952@gmail.com>
2025-07-25 22:23:47 +0530

    removed path*

--- Backend/server.js
+++ Backend/server.js
@@ -3,12 +3,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const path = require('path');
+
 
 const authRoutes = require("./routes/authRoutes");
 const noteRoutes = require("./routes/noteRoutes");
 const adminRoutes = require("./routes/adminRoutes");
-
 
 const app = express();
 
@@ -16,14 +15,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads")); // serve static PDF files
-app.use(express.static(path.join(__dirname, '../Frontend/dist')));
+
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
 app.use("/api/admin", adminRoutes);
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
-});
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
